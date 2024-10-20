4878b1ce4208d3bb9e71d4d9e0859c4ef2c6129f
ykkkk23 <teoyk9810@gmail.com>
2024-10-20 21:15:28 +0800

    env setup

--- ppo.py
+++ ppo.py
@@ -1,1 +1,15 @@
-print ("Commit test")
+import gymnasium as gym
+
+
+env = gym.make('CartPole-v1', render_mode='human')
+env.reset()
+
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+        
+env.close()
